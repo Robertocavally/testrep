c487df28c491644a93be031312b5ed24a10489b4
Robertocavally <119915258+Robertocavally@users.noreply.github.com>
2022-12-19 08:11:21 +0300

    shpargalka
    shpargalka

---
+++ shpargalka assert.sh
@@ -0,0 +1,40 @@
+assert_eq takes two strings and checks whether they are the same based on the character strings.
+assert_not_eq is the opposite of assert_eq.
+assert_true takes a parameter and returns 0 confirming the parameter is true.
+assert_false takes a parameter and decides whether it is false.
+assert_array_eq takes two arrays and compare them by items.
+assert_array_not_eq takes two arrays and return 0 if the items are not the same on the same index.
+assert_empty takes a string and returns 0 if it is empty
+assert_not_empty is the opposite of assert_empty.
+assert_contain checks whether the first argument contains the second one.
+assert_not_contain check whether the first argument does not contain the second one.
+assert_gt checks whether the first param is greater than the second.
+assert_ge checks whether the first param is greator than or equal to the second one.
+assert_lt checks whether the first param is less than the second one.
+assert_le checks whether the first param is less than or equal to the second one.
+
+How to write tests
+Example:
+source "./assert.sh"
+
+local expected actual
+expected="Hello"
+actual="World!"
+assert_eq "$expected" "$actual" "not equivalent!"
+# => x Hello == World :: not equivalent! 
+
+source "./assert.sh"
+
+local expected actual
+expected="Hello"
+actual="Hello"
+assert_eq "$expected" "$actual"
+if [ "$?" == 0 ]; then
+  log_success "assert_eq returns 0 if two words are equal"
+else
+  log_failure "assert_eq should return 0"
+fi
+
+
+
+If the return status ($?) of assert_eq is equal to 0, which is considered true according to the convention. If the assert function returns 1, the expected and actual values are differred.
